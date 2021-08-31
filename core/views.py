eb858dea6aca06bed1ae21b92cad0f721b26d44c
Diogo Lima Lucas <66488127+diogolimalucasdev@users.noreply.github.com>
2021-08-31 11:39:19 -0300

    Incluido Função e tela de auntenticação

--- core/views.py
+++ core/views.py
@@ -1,17 +1,46 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-#esse é um metodo de redirecionar mas vou utlizar o que é diretamente pela url
-#def index(req):
-  #  return redirect('/agenda/')
+
+# esse é um metodo de redirecionar mas vou utlizar o que é diretamente pela url
+# def index(req):
+#  return redirect('/agenda/')
 
 
 def Eventos(req, tituloEvento):
     return HttpResponse(f'Nome do evento: {tituloEvento} ')
 
 
+def login_user(req):
+    return render(req, 'login.html')
+
+
+def logout_user(req):
+    logout(req)
+    return redirect('/')  # quando entrar na função logout ele vai me recaminhar para o index
+
+
+def submit_login(req):
+    if req.POST:
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(req, usuario)
+            return redirect('/')
+        else:
+            messages.error(req, "Usuario ou senha invalido!")
+
+    return redirect('/')
+
+
+@login_required(login_url='/login/')  # pra quando o usuario nao estiver autenticado ele me levar para essa rota
 def lista_eventos(req):
-    evento = Evento.objects.all()
+    usuario = req.user
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(req, 'agenda.html', dados)
 
